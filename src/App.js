3da354273b53a6d04b7dc128f7c8d8e622f910a4
uslily <uslily@naver.com>
2020-08-11 21:12:04 +0900

    살려주세요

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+        <div>
+  <p> 안녕하세요! </p>
+   <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+        </div>
+  );
+}
+
+export default App;
